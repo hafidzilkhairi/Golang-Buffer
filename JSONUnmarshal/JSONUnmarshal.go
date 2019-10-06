a1cafd392ba26a98f9c81dc5e76b18847f66ebd6
Hafidzil Khairi <hafidzilkhairi00@gmail.com>
2019-10-06 22:49:51 +0700

    Wrote all of asked code

---
+++ JSONUnmarshal/JSONUnmarshal.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+)
+
+type Person struct {
+	FirstName string `json:"firstName"`
+	LastName  string `json:"lastName"`
+}
+
+func main() {
+	in := `{"firstName":"John", "lastName":"Doe"}`
+	bytes := []byte(in)
+
+	var p Person
+	err := json.Unmarshal(bytes, &p)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("%+v\n", p)
+}
